dd0cffbaacdd508fc9dab8177710f86699efe4ff
SyedAhkam <smahkam57@gmail.com>
2023-11-05 23:38:04 +0530

    fix: shadcn configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { ThemeProvider } from "@/components/ThemeProvider"
-import FileUpload from './components/FileUpload'
-
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <h1 className='text-xl text-red-500 font-bold'>Vite + React</h1>
-      <div>
+    <ThemeProvider defaultTheme="system" storageKey="ui-theme">
+      <h1 className="text-xl text-red-500 font-bold">Vite + React</h1>
       <Button>Click me</Button>
-      <FileUpload></FileUpload>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
     </ThemeProvider>
-      
-      
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
