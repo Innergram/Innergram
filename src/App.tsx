d17e82cdc772452adfaa07b825d970dc1b978175
Manya Raghuvanshi <manyaraghuvanshi66@gmail.com>
2023-11-05 16:15:07 +0530

    adding themes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
-
-
+import { ThemeProvider } from "@/components/ThemeProvider"
+import FileUpload from './components/FileUpload'
 
 
 function App() {
@@ -9,13 +9,17 @@ function App() {
 
   return (
     <>
-      <h1 className='text-xl text-red-500 font-bold'>Vite + React</h1>
+     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+     <h1 className='text-xl text-red-500 font-bold'>Vite + React</h1>
       <div>
       <Button>Click me</Button>
+      <FileUpload></FileUpload>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+    </ThemeProvider>
+      
       
     </>
   )
