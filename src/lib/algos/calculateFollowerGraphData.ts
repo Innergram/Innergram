3eec2066ac3417e00eba8970d699485695de15df
SyedAhkam <smahkam57@gmail.com>
2024-03-10 17:52:12 +0530

    refactor: remove unnecessary console.log

--- src/lib/algos/calculateFollowerGraphData.ts
+++ src/lib/algos/calculateFollowerGraphData.ts
@@ -4,24 +4,36 @@ import Follower from "@/interfaces/Follower";
 
 const FOLLOWER_WINDOW_SIZE = 86400 * 7; // 7 days in seconds
 
-export default function calculateFollowerGraphData(followers: Follower[] | undefined) {
-  const sortedFollowers = followers?.sort((a, b) => a.followed_at - b.followed_at);
+export default function calculateFollowerGraphData(
+  followers: Follower[] | undefined,
+) {
+  const sortedFollowers = followers?.sort(
+    (a, b) => a.followed_at - b.followed_at,
+  );
 
-  let result: { x: string, y: number, followersAtPresent: number }[] = [];
+  const result: { x: string; y: number; followersAtPresent: number }[] = [];
   sortedFollowers?.forEach((follower, idx) => {
     const currentTime = dayjs.unix(follower.followed_at);
     const windowStart = currentTime.subtract(FOLLOWER_WINDOW_SIZE * 1000);
 
     // Filter followers within the time window
     const followersInWindow = sortedFollowers.filter(
-      (f) => f.followed_at >= windowStart.unix() && f.followed_at <= currentTime.unix()
+      (f) =>
+        f.followed_at >= windowStart.unix() &&
+        f.followed_at <= currentTime.unix(),
     );
 
-    const uniqueFollowers = [...new Set(followersInWindow.map((f) => f.username))];
-    const growthDifference = idx > 0 ? uniqueFollowers.length - result[idx - 1].y : 0;
+    const uniqueFollowers = [
+      ...new Set(followersInWindow.map((f) => f.username)),
+    ];
+    const growthDifference =
+      idx > 0 ? uniqueFollowers.length - result[idx - 1].y : 0;
 
-    result.push({ x: currentTime.format("MMMM YYYY"), y: uniqueFollowers.length, followersAtPresent: idx + growthDifference });
-    console.log(result,"rizzzzzzz")
+    result.push({
+      x: currentTime.format("MMMM YYYY"),
+      y: uniqueFollowers.length,
+      followersAtPresent: idx + growthDifference,
+    });
   });
 
   return result;
